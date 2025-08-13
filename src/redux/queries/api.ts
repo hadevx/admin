7a56f59985fc2d531e35e6c34892cea3d69d6c7b
Hussain <hkosaimi@gmail.com>
2025-08-13 13:48:13 +0300

    fixing backend route

--- src/redux/queries/api.ts
+++ src/redux/queries/api.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:4001",
+  baseUrl: "https://backend-wxs4.onrender.com",
   credentials: "include",
 });
 
