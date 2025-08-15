2e4c8b7c1989b343baf9f2a8a15f76dfd267e924
Hussain <hkosaimi@gmail.com>
2025-08-15 09:07:12 +0300

    new

--- src/redux/queries/api.ts
+++ src/redux/queries/api.ts
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
   // baseUrl: "https://backend-wxs4.onrender.com",
-  baseUrl: "http://localhost:4001",
-  // baseUrl: "https://backend-production-9357.up.railway.app",
+  // baseUrl: "http://localhost:4001",
+  baseUrl: "https://backend-production-9357.up.railway.app",
   credentials: "include",
 });
 
