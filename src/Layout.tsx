066ffc24224d5f18829c072c6e803178c8cc9306
Hussain <hkosaimi@gmail.com>
2026-02-12 04:19:17 +0300

    new

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,7 +1,6 @@
 import "react-toastify/dist/ReactToastify.css";
 import SideMenu from "./components/SideMenu";
 import { useSelector } from "react-redux";
-import ThemeToggle from "@/components/ThemeToggle";
 function Layout({ children }: { children: React.ReactNode }) {
   const lang = useSelector((state: any) => state.language.lang);
 
@@ -17,7 +16,7 @@ function Layout({ children }: { children: React.ReactNode }) {
     return (
       <div className="flex   lg:gap-5  bg-zinc-100 dark:bg-neutral-950 transition-all duration-500 ease-in-out">
         <SideMenu />
-        {/* <ThemeToggle /> */}
+
         {children}
       </div>
     );
